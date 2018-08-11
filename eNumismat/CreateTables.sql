a37ae87152343dbe400ea71134d8ff9e980d72a1
Christoph Caina <admin@caina.de>
2018-08-12 01:12:30 +0200

    .

--- eNumismat/CreateTables.sql
+++ eNumismat/CreateTables.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE `contacts`
+﻿CREATE TABLE IF NOT EXISTS `contacts`
 (
 	`id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	`name` TEXT NOT NULL,
@@ -12,4 +12,23 @@
     `phone` TEXT,
     `mobile` TEXT,
     `email` TEXT
+);
+
+CREATE TABLE IF NOT EXISTS `swaplist`
+(
+	`id`INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	`contacts_id` INTEGER NOT NULL,
+	`date` NUMERIC NOT NULL,
+	`swapdetails_id` INTEGER NOT NULL,
+	`tracking_code_in` TEXT,
+	`tracking_code_out` TEXT,
+	`rating` NUMERIC,
+	`swapstatus` TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `swapdetails`
+(
+	`id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	`coin_id_out` INTEGER NOT NULL,
+	`coin_id_in` INTEGER NOT NULL
 );
