d10d577ec87a5709694abfcefafe33fb7399c42f
Christoph Caina <Christoph.Caina@synatec.de>
2018-08-22 08:42:39 +0200

    comments in addressbook form.

---
+++ eNumismat/InputValidation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eNumismat
+{
+    // This Class should handle the input Validation for UserInput on ALL Forms
+    // For doing this, we need to know, what Input we have, where it comes from - and then showing the Error (red Txt-field etc.) needs to be done on the calling form.
+    class InputValidation
+    {
+        public bool ValidateData(string input, string validationType)
+        {
+            switch (validationType)
+            {
+                case "IsNullOrEmpty":
+
+                    if (string.IsNullOrEmpty(input))
+                    {
+                        return true;
+                    }
+                    
+                    break;
+            }
+            return false;
+            //return false;
+        }
+
+    }
+}
